524420af12cec6134efc48b55d2a6669762a5ac4
Matt Land <mland@vectra.ai>
2020-09-04 11:13:02 -0500

    Add support for binding to VPC, rds security group support for private databases

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 data "aws_partition" "current" {}
 data "aws_region" "current" {}
 data "aws_caller_identity" "current" {}
-data "aws_subnet" "firstsub" {  id = "${var.subnets_lambda[0]}" }
+data "aws_subnet" "firstsub" { id = var.subnets_lambda[0] }
 
 resource "aws_iam_role" "lambda_rotation" {
-  name = "${var.name}-rotation_lambda"
+  name               = "${var.name}-rotation_lambda"
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -24,7 +24,7 @@ EOF
 
 resource "aws_iam_policy_attachment" "lambdabasic" {
   name       = "${var.name}-lambdabasic"
-  roles      = ["${aws_iam_role.lambda_rotation.name}"]
+  roles      = [aws_iam_role.lambda_rotation.name]
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
@@ -36,7 +36,8 @@ data "aws_iam_policy_document" "SecretsManagerRDSMySQLRotationSingleUserRolePoli
       "ec2:DescribeNetworkInterfaces",
       "ec2:DetachNetworkInterface",
     ]
-    resources = [ "*",]
+    resources = [
+    "*", ]
   }
   statement {
     actions = [
@@ -50,64 +51,69 @@ data "aws_iam_policy_document" "SecretsManagerRDSMySQLRotationSingleUserRolePoli
     ]
   }
   statement {
-    actions = ["secretsmanager:GetRandomPassword"]
-    resources = ["*",]
+    actions = [
+    "secretsmanager:GetRandomPassword"]
+    resources = [
+    "*", ]
   }
 }
 
 resource "aws_iam_policy" "SecretsManagerRDSMySQLRotationSingleUserRolePolicy" {
   name   = "${var.name}-SecretsManagerRDSMySQLRotationSingleUserRolePolicy"
   path   = "/"
-  policy = "${data.aws_iam_policy_document.SecretsManagerRDSMySQLRotationSingleUserRolePolicy.json}"
+  policy = data.aws_iam_policy_document.SecretsManagerRDSMySQLRotationSingleUserRolePolicy.json
 }
 
 
 resource "aws_iam_policy_attachment" "SecretsManagerRDSMySQLRotationSingleUserRolePolicy" {
-  name       = "${var.name}-SecretsManagerRDSMySQLRotationSingleUserRolePolicy"
-  roles      = ["${aws_iam_role.lambda_rotation.name}"]
-  policy_arn = "${aws_iam_policy.SecretsManagerRDSMySQLRotationSingleUserRolePolicy.arn}"
+  name = "${var.name}-SecretsManagerRDSMySQLRotationSingleUserRolePolicy"
+  roles = [
+  aws_iam_role.lambda_rotation.name]
+  policy_arn = aws_iam_policy.SecretsManagerRDSMySQLRotationSingleUserRolePolicy.arn
 }
 
 resource "aws_security_group" "lambda" {
-    vpc_id = "${data.aws_subnet.firstsub.vpc_id}"
-    name = "${var.name}-Lambda-SecretManager"
-    tags = {
-        Name  = "${var.name}-Lambda-SecretManager"
-    }
-    egress {
-        from_port       = 0
-        to_port         = 0
-        protocol        = "-1"
-        cidr_blocks     = ["0.0.0.0/0"]
+  vpc_id = data.aws_subnet.firstsub.vpc_id
+  name   = "${var.name}-Lambda-SecretManager"
+  tags = {
+    Name = "${var.name}-Lambda-SecretManager"
+  }
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = [
+    "0.0.0.0/0"]
   }
 }
 
-variable "filename" { default = "rotate-code-mysql"}
+variable "filename" { default = "rotate-code-mysql" }
 resource "aws_lambda_function" "rotate-code-mysql" {
-  filename           = "${path.module}/${var.filename}.zip"
-  function_name      = "${var.name}-${var.filename}"
-  role               = "${aws_iam_role.lambda_rotation.arn}"
-  handler            = "lambda_function.lambda_handler"
-  source_code_hash   = filebase64sha256("${path.module}/${var.filename}.zip")
-  runtime            = "python2.7"
+  filename         = "${path.module}/${var.filename}.zip"
+  function_name    = "${var.name}-${var.filename}"
+  role             = aws_iam_role.lambda_rotation.arn
+  handler          = "lambda_function.lambda_handler"
+  source_code_hash = filebase64sha256("${path.module}/${var.filename}.zip")
+  runtime          = "python2.7"
   vpc_config {
-    subnet_ids         = "${var.subnets_lambda}"
-    security_group_ids = ["${aws_security_group.lambda.id}"]
+    subnet_ids         = var.subnets_lambda
+    security_group_ids = [aws_security_group.lambda.id, var.rds_security_group_id]
   }
-  timeout            = 30
-  description        = "Conducts an AWS SecretsManager secret rotation for RDS MySQL using single user rotation scheme"
+  timeout     = 30
+  description = "Conducts an AWS SecretsManager secret rotation for RDS MySQL using single user rotation scheme"
   environment {
-    variables = { #https://docs.aws.amazon.com/general/latest/gr/rande.html#asm_region
+    variables = {
+      #https://docs.aws.amazon.com/general/latest/gr/rande.html#asm_region
       SECRETS_MANAGER_ENDPOINT = "https://secretsmanager.${data.aws_region.current.name}.amazonaws.com"
     }
   }
 }
 
 resource "aws_lambda_permission" "allow_secret_manager_call_Lambda" {
-    function_name = "${aws_lambda_function.rotate-code-mysql.function_name}"
-    statement_id = "AllowExecutionSecretManager"
-    action = "lambda:InvokeFunction"
-    principal = "secretsmanager.amazonaws.com"
+  function_name = aws_lambda_function.rotate-code-mysql.function_name
+  statement_id  = "AllowExecutionSecretManager"
+  action        = "lambda:InvokeFunction"
+  principal     = "secretsmanager.amazonaws.com"
 }
 /* not yet available
 data "aws_iam_policy_document" "kms" {
@@ -221,34 +227,33 @@ resource "aws_kms_key" "secret" {
 POLICY
 }
 
-
-
 resource "aws_kms_alias" "secret" {
   name          = "alias/${var.name}"
-  target_key_id = "${aws_kms_key.secret.key_id}"
+  target_key_id = aws_kms_key.secret.key_id
 }
 
-
-
 resource "aws_secretsmanager_secret" "secret" {
-  description         = "${var.secret_description}"
-  kms_key_id          = "${aws_kms_key.secret.key_id}"
-  name                = "${var.name}"
-  rotation_lambda_arn = "${aws_lambda_function.rotate-code-mysql.arn}"
+  description = var.secret_description
+  kms_key_id  = aws_kms_key.secret.key_id
+  name        = var.name
+}
+
+resource "aws_secretsmanager_secret_rotation" "secret" {
+  secret_id           = aws_secretsmanager_secret.secret.id
+  rotation_lambda_arn = aws_lambda_function.rotate-code-mysql.arn
+
   rotation_rules {
-    automatically_after_days = "${var.rotation_days}"
+    automatically_after_days = var.rotation_days
   }
-  #tags                = "${var.tags}"
-  #policy =
 }
 
 resource "aws_secretsmanager_secret_version" "secret" {
   lifecycle {
     ignore_changes = [
-      "secret_string"
+      secret_string
     ]
   }
-  secret_id     = "${aws_secretsmanager_secret.secret.id}"
+  secret_id     = aws_secretsmanager_secret.secret.id
   secret_string = <<EOF
 {
   "username": "${var.mysql_username}",
--- variables.tf
+++ variables.tf
@@ -1,36 +1,50 @@
 variable "name" {
   description = "This name will be used as prefix for all the created resources"
+  type        = string
 }
 variable "secret_description" {
   description = "This field is the description for the secret manager object"
   default     = "secret manager for mysql/aurora"
+  type        = string
 }
 variable "rotation_days" {
   default     = 30
+  type        = number
   description = "How often in days the secret will be rotated"
 }
 variable "subnets_lambda" {
-  type = "list"
+  type        = list(string)
   description = "The subnets where the Lambda Function will be run"
 }
 variable "mysql_username" {
+  type        = string
   description = "The MySQL/Aurora username you chose during RDS creation or another one that you want to rotate"
 }
 variable "mysql_dbname" {
+  type        = string
   description = "The Database name inside your RDS"
 }
 variable "mysql_host" {
+  type        = string
   description = "The RDS endpoint to connect to your database"
 }
 variable "mysql_password" {
+  type        = string
   description = "The password that you want to rotate, this will be changed after the creation"
 }
 variable "mysql_port" {
-  default = 3306
+  default     = 3306
+  type        = string
   description = "In case you don't have your MySQL on default port and you need to change it"
 }
 variable "mysql_dbInstanceIdentifier" {
+  type        = string
   description = "The RDS Identifier in the webconsole"
+}
+variable "rds_security_group_id" {
+  type        = string
+  description = "Security group id the lambda can join for rds access. (allows ingress from self)"
+  default     = ""
 }
 /* Not yet available 
 variable "additional_kms_role_arn" {
